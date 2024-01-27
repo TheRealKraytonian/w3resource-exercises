0d206a77809f09040332fefa1b18a7bb4e0537e1
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 19:18:11 -0800

    exercise2

--- basic-exercises/Exercise2/Program.cs
+++ basic-exercises/Exercise2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise2
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("Enter a number");
+		double b = double.Parse(System.Console.ReadLine());
+		System.Console.WriteLine("Enter b number");
+		double a = double.Parse(System.Console.ReadLine());
+		System.Console.WriteLine($"{a} plus {b} is {a+b}");
+	}
+}
---
+++ basic-exercises/Exercise2/readme.md
@@ -0,0 +1,10 @@
+# Exercise 2:  Write a C# Sharp program to print the sum of two numbers.
+---
+
+# Sample Output:
+---
+> Enter a number
+5
+Enter b number
+6
+6 plus 5 is 11
