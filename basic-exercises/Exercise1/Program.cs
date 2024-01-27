eea61ce2fca668470f0d34a127ec8a361288b449
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 17:07:40 -0800

    exerise 1 complete

--- basic-exercises/Exercise1/Program.cs
+++ basic-exercises/Exercise1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Exercise1
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("Hello");
+
+		System.Console.WriteLine("KraytonianPapi!");
+	}
+}
---
+++ basic-exercises/Exercise1/readme.md
@@ -0,0 +1,10 @@
+# Exercise 1
+---
+
+> Write a C# Sharp program to print Hello and your name in a separate line.
+
+
+## Output
+---
+> Hello
+Kraytonianpapi!
