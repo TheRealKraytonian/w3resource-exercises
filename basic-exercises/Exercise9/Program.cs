9969d5afa77096ce62596c62fbd8d1fff008a83e
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 22:13:57 -0800

    exercise 8 complete

--- basic-exercises/Exercise9/Program.cs
+++ basic-exercises/Exercise9/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise9
+{
+	public static void Main()
+	{
+		Console.WriteLine("Input the first number:");
+		int varOne = int.Parse(Console.ReadLine());
+		Console.WriteLine("Input the second number:");
+		int varTwo = int.Parse(Console.ReadLine());
+		Console.WriteLine("Input the three number:");
+		int varThree = int.Parse(Console.ReadLine());
+		Console.WriteLine("Input the four number:");
+		int varFour = int.Parse(Console.ReadLine());
+		Console.WriteLine($"{varOne}, {varTwo}, {varThree}, {varFour} is: {(varOne + varTwo + varThree + varFour) / 4}");
+	}
+}
