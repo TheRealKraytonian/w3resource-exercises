58f5c7fd26e3f84b96927de503d7a03ebd61303a
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 22:45:48 -0800

    exercise10

--- basic-exercises/Exercise10/Program.cs
+++ basic-exercises/Exercise10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise10 
+{
+	public static void Main()
+	{
+		Console.WriteLine("Input the first number:");
+		int varOne = int.Parse(Console.ReadLine());
+		Console.WriteLine("Input the second number:");
+		int varTwo = int.Parse(Console.ReadLine());
+		Console.WriteLine("Input the third number:");
+		int varThree = int.Parse(Console.ReadLine());
+		
+		Console.WriteLine
+($"Result of specified numbers {varOne}, {varTwo}, and {varThree}, ({varOne} + {varTwo}) x {varThree} is {(varOne + varTwo) * varThree} and ({varOne} x {varTwo}) + ({varTwo} x {varThree}) is {(varOne*varTwo) + (varTwo*varThree)}");
+	}
+}
