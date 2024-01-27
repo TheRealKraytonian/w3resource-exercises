7d6f963afd2075bf24617bc0fb8ac4dc8d83fe40
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 19:51:35 -0800

    exercise-4 complete

--- basic-exercises/Exercise4/Program.cs
+++ basic-exercises/Exercise4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using System;
+
+public class Exercise2
+{
+	public static void Main()
+	{
+		System.Console.WriteLine($"{-1 + 4 * 6}");
+		System.Console.WriteLine($"{( 35+ 5 ) % 7}");
+		System.Console.WriteLine($"{14 + -4 * 6 / 11}");
+		System.Console.WriteLine($"{2 + 15 / 6 * 1 - 7 % 2}");
+	}
+}
+
