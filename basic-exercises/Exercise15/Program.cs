d68ac15583ccab1d3870a71611182436922b1765
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-27 10:38:43 -0800

    exercise 15 completed

--- basic-exercises/Exercise15/Program.cs
+++ basic-exercises/Exercise15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise15
+{
+
+	public static void Main()
+	{
+		 Console.WriteLine("Enter Text");
+		 string TestData = Console.ReadLine();
+		 int strLength = TestData.Length;
+		 Console.WriteLine($"Remove an index from {TestData}");
+		 int IndexInput = Int32.Parse(Console.ReadLine());
+		 Console.WriteLine(IndexInput);
+		 TestData = TestData.Remove(IndexInput, 1);
+		 Console.WriteLine(TestData);
+	}
+}
