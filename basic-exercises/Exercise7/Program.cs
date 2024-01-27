2d2add3c545b9811e97e8cdce45d9a75a3fa2ad4
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 21:15:49 -0800

    exercise7 complete

--- basic-exercises/Exercise7/Program.cs
+++ basic-exercises/Exercise7/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+public class Exercise7
+{
+	public static void Main()
+	{
+		Console.WriteLine("Input the first number:");
+		int varOne = int.Parse(Console.ReadLine());
+		Console.WriteLine("Input the second number:");
+		int varTwo = int.Parse(Console.ReadLine());
+		Console.WriteLine($"{varOne} + {varTwo} = {varOne + varTwo}");
+		Console.WriteLine($"{varOne} - {varTwo} = {varOne - varTwo}");
+		Console.WriteLine($"{varOne} x {varTwo} = {varOne * varTwo}");
+		Console.WriteLine($"{varOne} / {varTwo} = {varOne / varTwo}");
+		Console.WriteLine($"{varOne} mod {varTwo} = {varOne % varTwo}");
+	}
+}
