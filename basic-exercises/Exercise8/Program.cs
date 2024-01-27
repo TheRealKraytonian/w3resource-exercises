ed6f8da9d9adac27beaf1fccd22f0a78e72136ff
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 22:04:15 -0800

    Exercise8

--- basic-exercises/Exercise8/Program.cs
+++ basic-exercises/Exercise8/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise8 
+{
+	public static void Main()
+	{ 
+		Console.WriteLine("Input the first number:");
+		int varOne = int.Parse(Console.ReadLine());
+		Console.WriteLine($"{varOne} * {0} = {varOne * 0}");
+		Console.WriteLine($"{varOne} * {1} = {varOne * 1}");
+		Console.WriteLine($"{varOne} * {2} = {varOne * 2}");
+		Console.WriteLine($"{varOne} * {3} = {varOne * 3}");
+		Console.WriteLine($"{varOne} * {4} = {varOne * 4}");
+		Console.WriteLine($"{varOne} * {5} = {varOne * 5}");
+		Console.WriteLine($"{varOne} * {6} = {varOne * 6}");
+		Console.WriteLine($"{varOne} * {7} = {varOne * 7}");
+		Console.WriteLine($"{varOne} * {8} = {varOne * 8}");
+		Console.WriteLine($"{varOne} * {9} = {varOne * 9}");
+		Console.WriteLine($"{varOne} * {10} = {varOne * 10}");
+	}
+}
