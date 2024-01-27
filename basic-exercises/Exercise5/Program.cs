8e6dc01eb25a57547f11768a8ecf6f500eedb984
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 20:16:08 -0800

    exercise 4 complete

--- basic-exercises/Exercise5/Program.cs
+++ basic-exercises/Exercise5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise5 
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("Enter first number to X:");
+		double? one = double.Parse(System.Console.ReadLine());
+		System.Console.WriteLine("Enter second number to X:");
+		double? two = double.Parse(System.Console.ReadLine());
+		System.Console.WriteLine("Enter third number to X:");
+		double? three = double.Parse(System.Console.ReadLine());
+		System.Console.WriteLine($"number {one} x number {two} x number {three} = {one*two*three}");
+	}	
+
+}
