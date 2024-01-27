64fdac004fcea70c68b99895e69f48442356b7a3
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-27 13:22:10 -0800

    Complete Exercise 16

--- basic-exercises/Exercise16/Program.cs
+++ basic-exercises/Exercise16/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise16
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter a Phrase");
+		char[] TestData = Console.ReadLine().ToCharArray();
+		(TestData[0],TestData[TestData.Length - 1]) = (TestData[TestData.Length - 1],TestData[0]);
+		Console.WriteLine(TestData);
+	}
+}       
