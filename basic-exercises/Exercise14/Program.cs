2d53d18dc557954f12859cfd6d45dabccbc93bff
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 23:42:33 -0800

    exercise 14 complete

--- basic-exercises/Exercise14/Program.cs
+++ basic-exercises/Exercise14/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise14
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter the amount of celsius:");
+		int celsius = int.Parse(Console.ReadLine());
+		Console.WriteLine($"Kelvin = {Math.Floor(celsius + 273.15)}");
+		Console.WriteLine($"Fahrenheit = {Math.Floor(celsius * 1.8 + 32)}");
+	}
+}
