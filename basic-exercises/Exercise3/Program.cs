05153923c4e41cbdafe6492652d18d15cfcf38dc
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 19:34:45 -0800

    exercise 3 complete

--- basic-exercises/Exercise3/Program.cs
+++ basic-exercises/Exercise3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise2
+{
+	public static void Main()
+	{
+ 		System.Console.WriteLine("Enter Numerator");
+		double a = double.Parse(System.Console.ReadLine());
+		System.Console.WriteLine("Enter Denominator");
+		double b = double.Parse(System.Console.ReadLine());
+		System.Console.WriteLine($"{a} / {b} is {a/b}");
+	}
+}
+
---
+++ basic-exercises/Exercise3/readme.md
@@ -0,0 +1,9 @@
+# Write a C# Sharp program to print the result of dividing two numbers.
+---
+## Sample Output
+---
+>Enter Numerator
+40
+Enter Denominator
+5
+40 / 5 is 8
