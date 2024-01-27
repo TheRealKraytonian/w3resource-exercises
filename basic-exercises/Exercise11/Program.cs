3fc1f9b2b3a9112e2fd5081ba7d653dab10eee36
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 22:52:51 -0800

    exercise11 complete

--- basic-exercises/Exercise11/Program.cs
+++ basic-exercises/Exercise11/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise11
+{
+	public static void Main()
+	{
+		Console.WriteLine("What's your age?");
+		var age = Console.ReadLine();
+		Console.WriteLine($"You look older than - {age}");
+	}
+}
