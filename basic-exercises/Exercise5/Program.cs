08a313524e2a14467b9d2791e37d9506485713ac
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-26 20:34:26 -0800

    exercise6 was 5 so i fixed it an now everything is in order true e5 and e6

--- basic-exercises/Exercise5/Program.cs
+++ basic-exercises/Exercise5/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 
-public class Exercise5 
+public class Exercise5
 {
 	public static void Main()
 	{
-		System.Console.WriteLine("Enter first number to X:");
-		double? one = double.Parse(System.Console.ReadLine());
-		System.Console.WriteLine("Enter second number to X:");
-		double? two = double.Parse(System.Console.ReadLine());
-		System.Console.WriteLine("Enter third number to X:");
-		double? three = double.Parse(System.Console.ReadLine());
-		System.Console.WriteLine($"number {one} x number {two} x number {three} = {one*two*three}");
-	}	
 
+		Console.WriteLine("Variable 1:");
+		var varOne = Console.ReadLine();
+		Console.WriteLine("Variable 2:");
+		var varTwo = Console.ReadLine();
+		Console.WriteLine($"Before swap - v1:{varOne} v2:{varTwo}");
+		var tempVar = varOne;
+		varOne = varTwo;
+		varTwo = tempVar;
+		Console.WriteLine($"After swap - v1:{varOne} v2:{varTwo}");
+		
+	}
 }
