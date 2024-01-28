25897daac51e70e8153a4eda1e1f8035be4bc94a
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-27 20:56:51 -0800

    e21 complete

--- basic-exercises/Exercise21/Program.cs
+++ basic-exercises/Exercise21/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise21
+{
+	public static void Main()
+	{
+		Console.WriteLine("number a");
+		int x = int.Parse(Console.ReadLine());
+		Console.WriteLine("number b");
+		int y = int.Parse(Console.ReadLine());
+		Console.WriteLine(x == 20 || y == 20 || x + y == 20);
+	}
+}
