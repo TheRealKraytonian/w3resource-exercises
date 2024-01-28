56c926b6edc67ee3b0c97ce58f85c09b064b09e2
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-27 16:43:25 -0800

    complete 18

--- basic-exercises/Exercise18/Program.cs
+++ basic-exercises/Exercise18/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class exercise18
+{
+	public static void Main()
+	{
+		Console.WriteLine("input first integer:");
+		int x = int.Parse(Console.ReadLine());
+		Console.WriteLine("input second integer:");
+		int y = int.Parse(Console.ReadLine());
+		 Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+	}
+}
