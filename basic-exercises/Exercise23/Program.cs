8eae1c441803da5da62fe47bbf79dd909f151019
TheRealKraytonian <oaccounts@chingarande.com>
2024-01-30 00:49:21 -0800

    e23 done

--- basic-exercises/Exercise23/Program.cs
+++ basic-exercises/Exercise23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise23
+{
+	public static void Main()
+	{
+		LowerCaseF("Write a C# Sharp Program to display the following pattern using the alphabet.");
+	}
+	
+	public static string LowerCaseF(string sent)
+	{
+		Console.WriteLine(sent.ToLower());
+		return sent;
+	}
+}
