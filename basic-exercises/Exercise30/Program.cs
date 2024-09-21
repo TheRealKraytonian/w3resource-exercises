3f12363eeae280038f8c020d7b65edd2a99bec9d
kraytonian <oaccounts@chingarande.com>
2024-09-20 23:18:28 -0700

    Exercise 30

--- basic-exercises/Exercise30/Program.cs
+++ basic-exercises/Exercise30/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercise
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Give me a hexadecimal number");
+
+        string hexaDec = Console.ReadLine();
+
+        int num = Int32.Parse(hexaDec, System.Globalization.NumberStyles.HexNumber);
+
+        Console.WriteLine("{0}", num);
+    }
+}
